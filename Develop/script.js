2a7b1014ccdf8f50375e240cf6126f1d37c3f714
Haley Telega <haleytelega@outlook.com>
2022-03-05 13:03:28 -0500

    First commit

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+// Assignment code here
+function generatePassword (){
+  window.prompt("Please select criteria for your secure password through the next few prompts");
+};
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
