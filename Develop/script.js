dcf8fe95177a559aeec7c677f656614e2e841583
Haley Telega <haleytelega@outlook.com>
2022-03-05 13:30:55 -0500

    first commit for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,22 @@
 // Assignment code here
+var passwordLength = function () {
+  var value = Math.floor(Math.random() * 8 + 120);
+
+  return value;
+}
 
 //generate alert when user clicks on Generate Password button
 function generatePassword (){
   window.alert("Please select criteria for your secure password through the next few prompts");
 };
+
+//alert asking for the password length
+function passwordCritera (){
+  var promptLength = window.alert("How many characters would you like your password to be? Please select from 8-128.");
+  if (passwordLength === 8)
+    window.alert("Thank you");
+    return passwordCritera();
+}
 
 
 // Get references to the #generate element
