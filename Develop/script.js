bf0410015c577142e49d1a9e90e2833bceefe6ec
Haley Telega <haleytelega@outlook.com>
2022-03-05 15:28:53 -0500

    second commit for password-length

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,30 @@
 // Assignment code here
-var passwordLength = function () {
-  var value = Math.floor(Math.random() * 8 + 120);
-
-  return value;
-}
-
-//prompt asking for the password length
-var getPasswordLength = function (){
-    var length = window.prompt("How many characters would you like your password to be? Please type a number from 8-128.");
-//   if (passwordLength === 8){
-//   window.alert("Thank you");
-//    console.log("Password Length:");
-//
-  return getPasswordLength();
-//}
-//  else {
-//    window.alert("Please type in a number between 8-128.")
-//    }
-}
 
 //generate alert when user clicks on Generate Password button
 function generatePassword (){
   window.alert("Please select criteria for your secure password through the next few prompts.");
 };
+
+generatePassword();
+
+
+//prompt asking for the password length
+var getPasswordLength = function (){
+    var length = window.prompt("How many characters would you like your password to be? Please type a number from 8-128.");
+    if (length >= 8 && length <= 128) {
+    window.alert("Thank you");
+    console.log("Password Length:" + length);
+
+//    return true;
+}
+  else {
+    window.alert("Please type in a number between 8-128.");
+    console.log("Invalid Response ");
+    }
+}
+
+getPasswordLength();
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -38,3 +40,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
