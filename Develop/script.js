168d575580c187393adb764d588ac289a4ba37f3
Haley Telega <haleytelega@outlook.com>
2022-03-06 19:03:30 -0500

    added my earlier functions into one function

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,40 @@
 // Assignment code here
+var length = '';
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var special = [" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
 
+
 function generatePassword () {
-  
+  window.prompt("How many characters would you like your password to be? Please type a number from 8-128.");
+  if (length >= 8 && length <= 128) {
+    console.log("Password Length:" + length);
+    }
+  if (lower == 'Yes' || 'yes') {
+    window.prompt("Do you want lower case letters in your password? Please type Yes or No.");
+    console.log("You selected lower.");
+  }
+  if (upper == 'Yes' || upper == "yes") {
+    window.prompt("Do you want upper case letters in your password? Please type Yes or No.");
+    console.log("You selected upper.");
+  }
+  if (numeric == 'Yes' || numeric == "yes"){
+    window.prompt("Do you want numbers in your password? Please type Yes or No.")
+    console.log("You selected numbers.");
+  }
+  if (special == 'Yes' || special == 'yes'){
+    window.prompt("Do you want special characters in your password? Please type in yes or no.");
+    console.log("You selected characters.");
+  }
+  if (length <= 0 && length >= 128 && lower == 'No' || lower == 'no' && upper == 'No' || upper == 'no' || numeric == 'No' || numeric == 'no' && special == 'No' || special == 'no')
+    console.log("User selected no");
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
