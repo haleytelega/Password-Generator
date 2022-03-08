f3381c705065a013b69527a5bbb84289d90e408f
Haley Telega <haleytelega@outlook.com>
2022-03-07 19:29:10 -0500

    finished the prompts and making sure the responses are correct

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-var length = '';
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -8,43 +7,29 @@ var special = [" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
 
 function generatePassword () {
   var length = window.prompt("How many characters would you like your password to be? Please type a number from 8-128.");
-  while (length >= 8 && length <= 128) {
+  console.log('length', length);
 
-  if (length <= 8 && length >= 128) {
+  if (length <= 8 || length >= 128) {
     window.alert("Please type out a number from 8-128. Thank you.");
+    generatePassword();
+  } 
+  else {
+  var useLower = window.confirm("Do you want lowercase letters in your password?");
+  var useUpper = window.confirm("Do you want uppercase letters in your password? ");
+  var useNumeric =  window.confirm("Do you want numbers in your password?");
+  var useSpecial = window.confirm("Do you want special characters in your password?");
+
+  
+
+
+
+
+
+  if (useLower == false && useUpper == false && useNumeric == false && useSpecial == false) {
+    window.alert("You must select either lowercase, uppercase, number or special character.");
+  generatePassword();
   }
 }
-  if (window.confirm("Do you want lowercase letters in your password?")) {
-      lower == true;
-      console.log("You selected lower.");
-  }
-  else {
-    console.log("You selected no.")
-  }
-  if (window.confirm("Do you want uppercase letters in your password? ")) {
-    upper == true;
-    console.log("You selected upper.");
-  }
-  else {
-    console.log("You selected no.")
-  }
-  if (window.confirm("Do you want numbers in your password?")) {
-    numeric == true;
-    console.log("You selected numbers.");
-  }
-  else {
-    console.log("You selected no.")
-  }
-  if (window.confirm("Do you want special characters in your password?")) {
-    special == true;
-    console.log("You selected characters.");
-  }
-  else {
-    console.log("You selected no.")
-  }
-  if (lower == false && upper == false && numeric == false && special == false) {
-    window.alert("You must select either lowercase, uppercase, number or special character.");
-  }
 }
 
 
